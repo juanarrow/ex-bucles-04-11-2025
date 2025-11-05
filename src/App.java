91345336e91203697ba3f1ae0293771ea80fd6fd
Juan A. García <juan.garcia@aliqindoi.com>
2025-11-05 16:33:14 +0100

    first commit

--- src/App.java
+++ src/App.java
@@ -1,9 +1,16 @@
 public class App {
+    public static final String RESET = "\033[0m" ;
+    public static final String RED_BACKGROUND = "\033[41m"; 
+    public static final String GREEN_BACKGROUND = "\033[42m";
+    public static final String BLUE_BACKGROUND = "\033[44m";
+    public static final String PURPLE_BACKGROUND = "\033[45m";   
     public static void main(String[] args) throws Exception {
         System.out.println("Introduzca el valor del intervalo mínimo: ");
         long min = 0;//Long.parseLong(System.console().readLine());
         System.out.println("Introduzca el valor del intervalo máximo: ");
         long max = 0;//Long.parseLong(System.console().readLine());
+        System.out.println("¿Blanco y negro o color (b|c)?: ");
+        boolean bn = System.console().readLine().toLowerCase().charAt(0)=='b';
         if (min <0 || max < 0){
             System.out.println("Deve introducir un intervalo correcto positivo donde min <=max");
             return;
@@ -14,7 +21,7 @@ public class App {
             max = aux;
         }
         long numero = (long)(Math.floor(Math.random()*(max - min +1)) + min);
-        numero = 123456765432100L;//12345678987654321001//
+        numero = 12345678987654321L;//12345678987654321001//
         numero = numero*10 +1;
         int digitos = 0;
         int digitoMayor = 0;
@@ -31,11 +38,63 @@ public class App {
         digitos--;
         //volteado -> 10012345678987654321
         aux = volteado;
-        int digitosAux = digitos;
-        while((digitosAux-- > 0)){
+        int fila = 1;
+        while((fila<=digitos)){
             int digito = (int)(aux%10);
             aux/=10;
-            System.out.println(digito);
+            if(bn){
+                for(int i = 1; i<=digitoMayor+1;i++)
+                    System.out.print(" ---");
+                System.out.println("");
+                for(int i = 1; i<=digitoMayor+1;i++){                
+                    System.out.printf("| %s ",
+                    (i==1)?digito:(i<=digito+1)?"*":" ");
+
+                }
+                System.out.println("|");
+            }
+            else{
+                for(int i = 1; i<=digitoMayor+2;i++)
+                    System.out.printf(
+                        "%s", 
+                        (fila==1)?
+                           //no es la ultima?()
+                          ((i!=digitoMayor+2)?(i==1)?"╔":"═╦":"═╗"):
+                        (fila!=digitos+1)?
+                          ((i!=digitoMayor+2)?(i==1)?"╠":"═╬":"═╣"):
+                          ((i!=digitoMayor+2)?(i==1)?"╚":"═╩":"═╝"));
+                    
+                System.out.println("");
+                for(int i = 1; i<=digitoMayor+1;i++){  
+                                  
+                    System.out.printf("║%s",
+                    (i==1)?digito:(i<=digito+1)?
+                    switch(digito){
+                        case 1,2,3,4->RED_BACKGROUND;
+                        case 5,6->GREEN_BACKGROUND;
+                        case 7,8->BLUE_BACKGROUND;
+                        case 9,10->PURPLE_BACKGROUND;
+                        default->"";
+                    }+" "+RESET:" ");
+
+                }
+                System.out.println("║");
+
+            }
+            fila++;
+            
         }
+        if(bn){
+            for(int i = 1; i<=digitoMayor+1;i++)
+                System.out.print(" ---");
+        }
+        else{
+            System.out.print("╚");
+            for(int i = 1; i<=digitoMayor;i++)
+                System.out.print("═╩");
+            System.out.print("═╝");
+            
+        }
+        
     }
 }
